6fc73f6175055ab0ddca861c1d1a91cc8f1b39ef
Pedro Eusebio <pedrosantos95@gmail.com>
2014-09-13 00:38:40 -0300

    - sending main.cpp alterations

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-	string line;
+	/*string line;
 	int n,x,y;
 	char * value = NULL;
 	vector<bool> bitMatrix;
@@ -19,13 +19,13 @@ int main()
 	if (true){
 		//graphMatrix= new bool*[n-1];
 		bitMatrix.resize(n*n, false);
-		/*for ( int i = 0; i< n-1; i++){
+		for ( int i = 0; i< n-1; i++){
 			//graphMatrix[i] = new bool [n-1];
 			for( int j = 0 ; j < n-1 ; j++){
 				//graphMatrix[i][j] = false;
 				cout<<bitMatrix[1*n+1]<<endl;
 			}
-		}*/	
+		}	
 		while (!file.eof()){
 			getline(file,line);
 			//if (value) delete value;
@@ -44,7 +44,54 @@ int main()
 		}
 	}
 	
-	cout<<bitMatrix[9*n+249]<<endl;
+	cout<<bitMatrix[9*n+249]<<endl;*/
+
+	string line;
+	int nVertices,x,y, mEdges;
+	float d_medio;
+
+	mEdges = 0;
+	d_medio = 0.0;
+	char * value = NULL;
+	vector<bool> bitMatrix;
+	ifstream file ("as_graph.txt" , ios::in|ios::binary);
+	getline (file,line);
+	sscanf(line.c_str(), "%d", &nVertices);
+
+	bitMatrix.resize(nVertices*nVertices,false); //resizing the bitMatrix to a fix size
+	int vertDegree[nVertices];//for each vertex, store it's degree
+	int degrees[nVertices-1]; //for each degree, indicate # of vertices that has it
+		
+	while (!file.eof()){
+		getline(file,line);
+		sscanf(line.c_str(), "%d %d", &x, &y);
+		x--;y--;// trying not fill the index 0.
+		bitMatrix[x*nVertices+y] = true;
+		bitMatrix[y*nVertices+x] = true;// redundance
+		mEdges++;
+		vertDegree[x]++;
+		vertDegree[y]++;
+		d_medio+=2.0;
+	}
+
+	file.close();
+
+	for (int j = 0; j<nVertices; j++){
+		degrees[vertDegree[j]]++;
+	}
+
+
+	string degreeString;
+	for (int w= 0; w<nVertices; w++){
+		degreeString+= std::to_string(w)+ std::to_string(degrees[w]/nVertices)+"\n";
+	}	
+
+	d_medio = d_medio/nVertices;
+
+	ofstream outFile;
+	outFile.open("./graphInfo.txt");
+	outFile<<"#n = "<<nVertices<<"\n"<<"#m = "<<mEdges<<"\n"<<"#d_medio = "<<d_medio<<"\n"<<degreeString<<endl;
+	outFile.close();
 
 }
 	
